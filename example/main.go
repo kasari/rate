9adb9b13ed865f727226b61196c6e87278436f88
kasari <kasari3939@gmail.com>
2017-01-04 19:58:54 +0900

    first commit

---
+++ example/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/kasari/rate"
+)
+
+const TryCount = 20000
+
+func main() {
+	data := []rate.RateValue{
+		rate.RateValue{10, "rate-10"},
+		rate.RateValue{30, "rate-30"},
+		rate.RateValue{60, "rate-60"},
+	}
+
+	r := rate.NewRate()
+	r.MaxRate = 200
+	r.DefaultValue = "default"
+	for _, d := range data {
+		err := r.Add(d.Rate, d.Value)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
+
+	countMap := make(map[interface{}]int)
+	for i := 0; i < TryCount; i++ {
+		v := r.Gene()
+		countMap[v]++
+	}
+
+	fmt.Printf("[TryCount: %d]\n", TryCount)
+	for k, v := range countMap {
+		fmt.Printf("%v: %d\n", k, v)
+	}
+}
---
+++ rate.go
@@ -0,0 +1,70 @@
+package rate
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+type RateValue struct {
+	Rate  float64
+	Value interface{}
+}
+
+type Option struct {
+	NoMaxRate bool
+}
+
+type Rate struct {
+	Option
+	MaxRate      float64
+	RateValues   []RateValue
+	DefaultValue interface{}
+	RandFunc     func() float64 // return number in [0.0,1.0)
+}
+
+func NewRate() *Rate {
+	return NewRateWithOption(Option{})
+}
+
+func NewRateWithOption(o Option) *Rate {
+	return &Rate{Option: o}
+}
+
+func (r *Rate) Add(rate float64, value interface{}) error {
+	if r.Option.NoMaxRate {
+		r.MaxRate += rate
+	}
+
+	var totalRate float64
+	for _, rv := range r.RateValues {
+		totalRate += rv.Rate
+	}
+
+	if totalRate+rate > r.MaxRate {
+		return fmt.Errorf("error exceed MaxRate, current:%f MaxRate:%f", totalRate+rate, r.MaxRate)
+	}
+
+	r.RateValues = append(r.RateValues, RateValue{rate, value})
+
+	return nil
+}
+
+func (r *Rate) Gene() interface{} {
+	if r.RandFunc == nil {
+		rand.Seed(time.Now().UnixNano())
+		r.RandFunc = rand.Float64
+	}
+
+	index := r.RandFunc() * r.MaxRate
+
+	var cursor float64
+	for _, rv := range r.RateValues {
+		cursor += rv.Rate
+		if index < cursor {
+			return rv.Value
+		}
+	}
+
+	return r.DefaultValue
+}
