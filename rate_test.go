9de4f54c1fed2b3f108e76d5775d5dc359b383cc
kasari <kasari3939@gmail.com>
2017-05-02 12:22:55 +0900

    test を厳密に

--- rate_test.go
+++ rate_test.go
@@ -4,28 +4,56 @@ import "testing"
 
 func TestAdd(t *testing.T) {
 	r := NewRate()
-	r.MaxRate = 100
 
-	if err := r.Add(30.0, "30.0"); err != nil {
-		t.Error("if not exceed MaxRate, not return error")
+	if len(r.RateValues) != 0 {
+		t.Fatal("default RateValues length is 0")
 	}
 
-	if err := r.Add(80.0, "80.0"); err == nil {
-		t.Error("if exceed MaxRate, return error")
+	r.Add(1.0, "1.0")
+
+	if len(r.RateValues) != 1 {
+		t.Fatal("Add add rate value to RateValues")
 	}
+
 }
 
 func TestGenerate(t *testing.T) {
 	r := NewRate()
-	r.DefaultValue = "default"
-	r.MaxRate = 100
-	r.RandFunc = func() float64 {
-		return 1.0
+
+	r.Add(30.0, "30")
+	r.Add(70.0, "70")
+
+	type Test struct {
+		Rand  float64
+		Value interface{}
+	}
+	tests := []Test{
+		Test{
+			Rand:  0.0,
+			Value: "30",
+		},
+		Test{
+			Rand:  0.29999,
+			Value: "30",
+		},
+		Test{
+			Rand:  0.3,
+			Value: "70",
+		},
+		Test{
+			Rand:  0.99999,
+			Value: "70",
+		},
 	}
 
-	r.Add(30.0, "30.0")
+	for _, test := range tests {
+		r.RandFunc = func() float64 {
+			return test.Rand
+		}
 
-	if r.Generate() != "default" {
-		t.Error("if not select set value, Generate returns DefaultValue")
+		value := r.Generate()
+		if value != test.Value {
+			t.Errorf("value %+v, want %+v", value, test.Value)
+		}
 	}
 }
